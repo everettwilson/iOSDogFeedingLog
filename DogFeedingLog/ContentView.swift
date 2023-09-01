acf7d857a12425388899f670a37451ab312fb159
everettwilson <everettwilson21@me.com>
2023-09-01 11:49:51 -0500

    Initial working version

--- DogFeedingLog/ContentView.swift
+++ DogFeedingLog/ContentView.swift
@@ -6,21 +6,75 @@
 //
 
 import SwiftUI
+import Foundation
 
 struct ContentView: View {
+    @State private var lastFeedingTime: String = "Not yet fetched"
+    @State private var errorMessage: String = ""
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
-}
+            Button("Log Feeding Time") {
+                // Your code to log feeding time will go here
+                logFeedingTime()
+            }
+            .padding()
+            .background(Color.blue)
+            .foregroundColor(.white)
+            .cornerRadius(8)
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+            Button("Get Last Feeding Time") {
+                // Your code to get the last feeding time will go here
+                getLastFeedingTime()
+            }
+            .padding()
+            .background(Color.green)
+            .foregroundColor(.white)
+            .cornerRadius(8)
+        }
+        Text("Last feeding time: \(lastFeedingTime)").padding()
+
+        if !errorMessage.isEmpty {
+            Text("Error: \(errorMessage)")
+        }
     }
+
+    func logFeedingTime() {
+        guard let url = URL(string: "https://7px8d2brr8.execute-api.us-west-2.amazonaws.com/prod/logfeedingtime") else { return }
+
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+
+        let task = URLSession.shared.dataTask(with: request) { data, _, error in
+            DispatchQueue.main.async {
+                if let error = error {
+                    print("Error: \(error)")
+                    self.errorMessage = "Failed to log data: \(error.localizedDescription)"
+                } else if let data = data, let str = String(data: data, encoding: .utf8) {
+                    print("Received data:\n\(str)")
+                    self.lastFeedingTime = str
+                }
+            }
+        }
+        task.resume()
+    }
+
+    func getLastFeedingTime() {
+        guard let url = URL(string: "https://7px8d2brr8.execute-api.us-west-2.amazonaws.com/prod/getlastfeedingtime") else { return }
+
+        let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+            DispatchQueue.main.async {
+                if let error = error {
+                    print("Error: \(error)")
+                    self.errorMessage = "Failed to get data: \(error.localizedDescription)"
+                } else if let data = data {
+                    let str = String(data: data, encoding: .utf8)
+                    print("Received data:\n\(str ?? "")")
+                    self.lastFeedingTime = str!
+                }
+            }
+        }
+        task.resume()
+    }
+    
 }
