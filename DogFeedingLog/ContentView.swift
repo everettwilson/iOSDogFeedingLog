fe77bfdd8f45498bb087b7f0b0bc97936f55c004
everettwilson <everettwilson21@me.com>
2023-08-31 22:28:03 -0500

    Initial Commit

---
+++ DogFeedingLog/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  DogFeedingLog
+//
+//  Created by Everett Wilson on 8/31/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DogFeedingLog/DogFeedingLogApp.swift
@@ -0,0 +1,17 @@
+//
+//  DogFeedingLogApp.swift
+//  DogFeedingLog
+//
+//  Created by Everett Wilson on 8/31/23.
+//
+
+import SwiftUI
+
+@main
+struct DogFeedingLogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
